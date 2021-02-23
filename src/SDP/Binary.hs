6f87304a6ab53134a66a9bb2b94bfbf81317a8ec
Andrey Mulik <work.a.mulik@gmail.com>
2021-02-23 10:52:13 +0300

    prepared for Hackage

--- src/SDP/Binary.hs
+++ src/SDP/Binary.hs
@@ -35,31 +35,21 @@ default ()
 
 instance (Binary e) => Binary (SArray# e)
   where
-    get = do n <- get; es <- getMany (max 0 n); return (fromListN n es)
+    get = do n <- get; fromListN n <$> replicateM n get
     put = putList . listL
 
 instance (Binary e, Unboxed e) => Binary (SBytes# e)
   where
-    get = do n <- get; es <- getMany (max 0 n); return (fromListN n es)
+    get = do n <- get; fromListN n <$> replicateM n get
     put = putList . listL
 
 instance (Nullable (rep e), Binary (rep e)) => Binary (AnyChunks rep e)
   where
     put = \ es -> let cs = toChunks es in do put (sizeOf cs); putList cs
-    get = fromChunks <$> (getMany =<< get)
+    get = do n <- get; fromChunks <$> replicateM n get
 
 instance (Binary i, Index i, Binary (rep e)) => Binary (AnyBorder rep i e)
   where
     put = \ (AnyBorder l u es) -> do put l; put u; put es
     get = liftA3 AnyBorder get get get
-
---------------------------------------------------------------------------------
-
-{-# INLINE getMany #-}
-getMany :: (Binary e) => Int -> Get [e]
-getMany =  go []
- where
-    go xs 0 = return $! reverse xs
-    go xs i = do x <- get; x `seq` go (x : xs) (i - 1)
-
 
