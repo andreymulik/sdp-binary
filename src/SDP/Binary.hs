b80e3155b0b6bd3b0ecc9984bc0d39cf4762a02d
Andrey Mulik <work.a.mulik@gmail.com>
2021-09-05 14:37:13 +0300

    fixed definition names ambiguity

--- src/SDP/Binary.hs
+++ src/SDP/Binary.hs
@@ -12,7 +12,7 @@
 module SDP.Binary
 (
   -- * Export
-  module Data.Binary
+  module B
 )
 where
 
@@ -27,7 +27,7 @@ import SDP.Templates.AnyChunks
 import SDP.Prim.SArray
 import SDP.Prim.SBytes
 
-import Data.Binary
+import Data.Binary as B
 
 default ()
 
@@ -35,21 +35,21 @@ default ()
 
 instance (Binary e) => Binary (SArray# e)
   where
-    get = do n <- get; fromListN n <$> replicateM n get
+    get = do n <- B.get; fromListN n <$> replicateM n B.get
     put = putList . listL
 
 instance (Binary e, Unboxed e) => Binary (SBytes# e)
   where
-    get = do n <- get; fromListN n <$> replicateM n get
+    get = do n <- B.get; fromListN n <$> replicateM n B.get
     put = putList . listL
 
 instance (Nullable (rep e), Binary (rep e)) => Binary (AnyChunks rep e)
   where
     put = \ es -> let cs = toChunks es in do put (sizeOf cs); putList cs
-    get = do n <- get; fromChunks <$> replicateM n get
+    get = do n <- B.get; fromChunks <$> replicateM n B.get
 
 instance (Binary i, Index i, Binary (rep e)) => Binary (AnyBorder rep i e)
   where
     put = \ (AnyBorder l u es) -> do put l; put u; put es
-    get = liftA3 AnyBorder get get get
+    get = liftA3 AnyBorder B.get B.get B.get
 
